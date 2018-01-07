a4fc9ccffbec053b59778fdf109cf0a5777485a6
Guillaume Tran <guillaume.tran@epitech.eu>
2018-01-07 07:10:08 +0100

    Add component backgroundPage

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { StyleSheet, Text, View, ImageBackground } from "react-native";
 import TouchableCard from "./src/TouchableCard/TouchableCard";
+import BackgroundPage from "./src/BackgroundPage/BackgroundPage";
 import { DangerZone } from "expo";
 const { Lottie } = DangerZone;
 
@@ -8,47 +9,60 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <View style={{ width: 330, height: 125 }}>
-          <TouchableCard
-            color="white"
-            borderRadius={10}
-            shadow={true}
-            shadowColor="blue"
-            animation={true}
-            gradient={true}
-            onPress={() => {}}
+        <BackgroundPage
+          backgroundContent={
+            <View style={{ flex: 1 }}>
+              <View style={{ flex: 1, backgroundColor: "transparent" }} />
+              <View style={{ flex: 2, backgroundColor: "blue" }} />
+            </View>
+          }
+        >
+          <View
+            style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
           >
-            <View
-              style={{
-                flex: 1,
-                borderRadius: 10,
-                flexDirection: "row",
-                overflow: "hidden"
-              }}
-            >
-              <View
-                style={{
-                  flex: 2
-                }}
-              />
-              <ImageBackground
-                style={{
-                  flex: 1,
-                  height: "100%"
-                }}
-                source={require("./src/assets/BlueBar_3.png")}
-                resizeMode="cover"
+            <View style={{ width: 330, height: 125 }}>
+              <TouchableCard
+                color="white"
+                borderRadius={10}
+                shadow={true}
+                shadowColor="blue"
+                animation={true}
+                gradient={true}
+                onPress={() => {}}
               >
                 <View
                   style={{
                     flex: 1,
-                    backgroundColor: "rgba(66, 117, 152, 0.4)"
+                    borderRadius: 10,
+                    flexDirection: "row",
+                    overflow: "hidden"
                   }}
-                />
-              </ImageBackground>
+                >
+                  <View
+                    style={{
+                      flex: 2
+                    }}
+                  />
+                  <ImageBackground
+                    style={{
+                      flex: 1,
+                      height: "100%"
+                    }}
+                    source={require("./src/assets/BlueBar_3.png")}
+                    resizeMode="cover"
+                  >
+                    <View
+                      style={{
+                        flex: 1,
+                        backgroundColor: "rgba(66, 117, 152, 0.4)"
+                      }}
+                    />
+                  </ImageBackground>
+                </View>
+              </TouchableCard>
             </View>
-          </TouchableCard>
-        </View>
+          </View>
+        </BackgroundPage>
       </View>
     );
   }
@@ -57,8 +71,6 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    justifyContent: "center",
-    alignItems: "center"
+    backgroundColor: "#fff"
   }
 });
